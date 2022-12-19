78fdc021840493a04b24f3250232f602b911cb7b
VSV <mr.vsv92@mail.ru>
2022-12-20 00:04:59 +0700

    оставляем пока что так. потом раскидаем по архитектуре

--- app/src/main/java/com/example/unsplashhomework/data/api/constant.kt
+++ app/src/main/java/com/example/unsplashhomework/data/api/constant.kt
@@ -1,15 +1,7 @@
 package com.example.unsplashhomework.data.api
 
-val KEY = "mj_2vu5NRd4iwh6pYzpqOymkmK79_WqclNutm5-O2UQ"
-val SECRET_KEY = "C_vgTr-b2b6-ZdhllFr35MO5uNLk5mxGmFpuoq4-Bt0"
+val KEY = "OOlbm6FJx2pFPfaU8uLWLbsQ1Ez0u1J-tXX5L8WLya8"
+val SECRET_KEY = "70SAVhXIiZ6ySujad4zR7b4-PZ9TymGOU_H_qDu-9_A"
 val authorization_uri = "urn:ietf:wg:oauth:2.0:oob"
 
-/*
-val call ="https://unsplash.com/oauth/authorize" +
-        "?client_id=OOlbm6FJx2pFPfaU8uLWLbsQ1Ez0u1J-tXX5L8WLya8&" +
-        "redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&" +
-        "response_type=code" +
-        "&scope=public+read_user+write_user+read_photos+write_photos+write_likes" +
-        "+write_followers+read_collections+write_collections"*/
-
-val call = "https://unsplash.com/oauth/authorize?client_id=mj_2vu5NRd4iwh6pYzpqOymkmK79_WqclNutm5-O2UQ&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=public+read_user+read_photos+write_likes+write_followers+read_collections"
+val call = "https://unsplash.com/oauth/authorize?client_id=OOlbm6FJx2pFPfaU8uLWLbsQ1Ez0u1J-tXX5L8WLya8&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code&scope=public+read_user+read_photos+write_likes+write_followers+read_collections"
