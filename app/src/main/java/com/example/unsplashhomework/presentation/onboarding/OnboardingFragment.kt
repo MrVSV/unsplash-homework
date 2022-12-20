986348b80984606a56a318ceba7310477ca5c6b6
rodin <rodin_sr@proton.me>
2022-12-20 22:40:03 +0300

    добавил две иконки индикаторов, коментарии для онбординг фрагмента, и адаптера

--- app/src/main/java/com/example/unsplashhomework/presentation/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/example/unsplashhomework/presentation/onboarding/OnboardingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.unsplashhomework.presentation.onboarding
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,24 +18,39 @@ class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        /** весь список можно вынести в рессуры и не создавать его тут а просто к переменно присвоть
+         * список из ресурсов */
         val texts =
             listOf(
                 getString(R.string.first_onboarding),
                 getString(R.string.second_onboarding),
                 getString(R.string.third_onboarding)
             )
+
         val adapter = ViewPagerAdapter(texts)
+        /** я уже вроде писал что биндинг нужен для того что бы вот так не писать
+         * меньше переменных всегда лучше каждое объявленная переменная отъедает память */
         val viewPager = binding.viewPager
         val tabs = binding.tabs
-
+        /** вот тут можно написать сразу binding.viewPager.adapter = ViewPagerAdapter(texts)
+         * тем самым мы избавимся от переменной адаптер которую мы больше нигде не юзаем */
         viewPager.adapter = adapter
 
+        /** вариант рабочий но мы тут дергаем ContextCompat не страшно,
+         * тут не какойто сложный таб, просто точка я как пример передел хml файл и запись в коде
+         * сократиться*/
+
+/*
         TabLayoutMediator(tabs, viewPager) { tab, _ ->
             tab.icon = ContextCompat.getDrawable(
                 requireContext().applicationContext,
                 R.drawable.ic_baseline_circle_24
             )
-        }.attach()
+        }.attach()*/
+        /** опять такж не стесняйся юзать биндинг это лучше чем переменную объявлять переменную
+         * с темже успехом можно findViewByID юзать*/
+        TabLayoutMediator(tabs, viewPager) { _, _ -> }.attach()
+
 
         binding.authorizeButton.setOnClickListener {
             findNavController().navigate(R.id.action_navigation_onboarding_to_navigation_auth)
--- app/src/main/java/com/example/unsplashhomework/presentation/onboarding/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/unsplashhomework/presentation/onboarding/ViewPagerAdapter.kt
@@ -5,6 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.unsplashhomework.databinding.ItemViewpagerBinding
 
+/** смотри ниже...binding должен быть приватным
+ * ия бы всетаки для холера отдельный класс создал*/
+
 class ViewPagerHolder(val binding: ItemViewpagerBinding) :
     RecyclerView.ViewHolder(binding.root)
 
@@ -20,12 +23,23 @@ class ViewPagerAdapter(
 
     override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
         val item = texts[position]
+        /** вот тут не надо так делать... у тебя во вью холдера должна быьть просто функция bind
+         * которя делать тоже самое а тут запись должна быть holder.bind а binding вообще должен
+         * быть приватным приватный
+         * и смотри езще момент у тебя действие вот тут конкретно в одну строчку зачем тут лишняя
+         * вложеность в виде with? если у тебя 7 действий с биндингом допустим то так даже нужно
+         * тут нет*/
         with(holder.binding) {
             onboardingTexts.text = item
         }
     }
 
+    /** ну это на вкус и цвет как говориться
+     *  override fun getItemCount(): =texts.size
+     *  такая запись лично мне нравится больше...А там смотрите сами. В проекте должно быть все
+     *  написано однотипно*/
     override fun getItemCount(): Int {
         return texts.size
     }
+
 }
