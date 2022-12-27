ece9808edad7ff620d9541f71ce6e8a2418a51fb
VSV <mr.vsv92@mail.ru>
2022-12-27 10:16:26 +0700

    мучаюсь с колапсинг тулбаром

---
+++ app/src/main/java/com/example/unsplashhomework/presentation/collections/DigestDetailsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.unsplashhomework.presentation.collections
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.example.unsplashhomework.databinding.FragmentDigestDetailsBinding
+import com.example.unsplashhomework.tools.BaseFragment
+
+class DigestDetailsFragment : BaseFragment<FragmentDigestDetailsBinding>() {
+
+
+    private val viewModel by viewModels<DigestDetailsViewModel>()
+
+    override fun initBinding(inflater: LayoutInflater) =
+        FragmentDigestDetailsBinding.inflate(inflater)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/unsplashhomework/presentation/collections/DigestDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.unsplashhomework.presentation.collections
+
+import androidx.lifecycle.ViewModel
+
+class DigestDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
